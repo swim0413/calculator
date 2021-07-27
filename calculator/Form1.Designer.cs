f787eeefe231a389ccb8df5947be475613d09a7e
swim0413 <chosuyoung0413@gmail.com>
2021-07-27 20:04:29 +0900

    초기 커밋.

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.numBt1 = new System.Windows.Forms.Button();
+            this.numBt2 = new System.Windows.Forms.Button();
+            this.numBt3 = new System.Windows.Forms.Button();
+            this.numBt4 = new System.Windows.Forms.Button();
+            this.numBt5 = new System.Windows.Forms.Button();
+            this.numBt6 = new System.Windows.Forms.Button();
+            this.numBt7 = new System.Windows.Forms.Button();
+            this.numBt8 = new System.Windows.Forms.Button();
+            this.numBt9 = new System.Windows.Forms.Button();
+            this.numBt0 = new System.Windows.Forms.Button();
+            this.plusBt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resultBox
+            // 
+            this.resultBox.Font = new System.Drawing.Font("맑은 고딕", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resultBox.Location = new System.Drawing.Point(12, 31);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(358, 53);
+            this.resultBox.TabIndex = 0;
+            this.resultBox.Text = "0";
+            this.resultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numBt1
+            // 
+            this.numBt1.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt1.Location = new System.Drawing.Point(23, 417);
+            this.numBt1.Name = "numBt1";
+            this.numBt1.Size = new System.Drawing.Size(70, 68);
+            this.numBt1.TabIndex = 1;
+            this.numBt1.Text = "1";
+            this.numBt1.UseVisualStyleBackColor = true;
+            this.numBt1.Click += new System.EventHandler(this.numBt1_Click);
+            // 
+            // numBt2
+            // 
+            this.numBt2.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt2.Location = new System.Drawing.Point(110, 417);
+            this.numBt2.Name = "numBt2";
+            this.numBt2.Size = new System.Drawing.Size(70, 68);
+            this.numBt2.TabIndex = 2;
+            this.numBt2.Text = "2";
+            this.numBt2.UseVisualStyleBackColor = true;
+            this.numBt2.Click += new System.EventHandler(this.numBt2_Click);
+            // 
+            // numBt3
+            // 
+            this.numBt3.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt3.Location = new System.Drawing.Point(198, 417);
+            this.numBt3.Name = "numBt3";
+            this.numBt3.Size = new System.Drawing.Size(70, 68);
+            this.numBt3.TabIndex = 3;
+            this.numBt3.Text = "3";
+            this.numBt3.UseVisualStyleBackColor = true;
+            // 
+            // numBt4
+            // 
+            this.numBt4.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt4.Location = new System.Drawing.Point(23, 332);
+            this.numBt4.Name = "numBt4";
+            this.numBt4.Size = new System.Drawing.Size(70, 68);
+            this.numBt4.TabIndex = 4;
+            this.numBt4.Text = "4";
+            this.numBt4.UseVisualStyleBackColor = true;
+            // 
+            // numBt5
+            // 
+            this.numBt5.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt5.Location = new System.Drawing.Point(110, 332);
+            this.numBt5.Name = "numBt5";
+            this.numBt5.Size = new System.Drawing.Size(70, 68);
+            this.numBt5.TabIndex = 5;
+            this.numBt5.Text = "5";
+            this.numBt5.UseVisualStyleBackColor = true;
+            // 
+            // numBt6
+            // 
+            this.numBt6.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt6.Location = new System.Drawing.Point(198, 332);
+            this.numBt6.Name = "numBt6";
+            this.numBt6.Size = new System.Drawing.Size(70, 68);
+            this.numBt6.TabIndex = 6;
+            this.numBt6.Text = "6";
+            this.numBt6.UseVisualStyleBackColor = true;
+            // 
+            // numBt7
+            // 
+            this.numBt7.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt7.Location = new System.Drawing.Point(23, 248);
+            this.numBt7.Name = "numBt7";
+            this.numBt7.Size = new System.Drawing.Size(70, 68);
+            this.numBt7.TabIndex = 7;
+            this.numBt7.Text = "7";
+            this.numBt7.UseVisualStyleBackColor = true;
+            // 
+            // numBt8
+            // 
+            this.numBt8.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt8.Location = new System.Drawing.Point(110, 248);
+            this.numBt8.Name = "numBt8";
+            this.numBt8.Size = new System.Drawing.Size(70, 68);
+            this.numBt8.TabIndex = 8;
+            this.numBt8.Text = "8";
+            this.numBt8.UseVisualStyleBackColor = true;
+            // 
+            // numBt9
+            // 
+            this.numBt9.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt9.Location = new System.Drawing.Point(198, 248);
+            this.numBt9.Name = "numBt9";
+            this.numBt9.Size = new System.Drawing.Size(70, 68);
+            this.numBt9.TabIndex = 9;
+            this.numBt9.Text = "9";
+            this.numBt9.UseVisualStyleBackColor = true;
+            // 
+            // numBt0
+            // 
+            this.numBt0.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numBt0.Location = new System.Drawing.Point(110, 501);
+            this.numBt0.Name = "numBt0";
+            this.numBt0.Size = new System.Drawing.Size(70, 68);
+            this.numBt0.TabIndex = 10;
+            this.numBt0.Text = "0";
+            this.numBt0.UseVisualStyleBackColor = true;
+            // 
+            // plusBt
+            // 
+            this.plusBt.Font = new System.Drawing.Font("맑은 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.plusBt.Location = new System.Drawing.Point(303, 332);
+            this.plusBt.Name = "plusBt";
+            this.plusBt.Size = new System.Drawing.Size(67, 68);
+            this.plusBt.TabIndex = 11;
+            this.plusBt.Text = "+";
+            this.plusBt.UseVisualStyleBackColor = true;
+            this.plusBt.Click += new System.EventHandler(this.plusBt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 577);
+            this.Controls.Add(this.plusBt);
+            this.Controls.Add(this.numBt0);
+            this.Controls.Add(this.numBt9);
+            this.Controls.Add(this.numBt8);
+            this.Controls.Add(this.numBt7);
+            this.Controls.Add(this.numBt6);
+            this.Controls.Add(this.numBt5);
+            this.Controls.Add(this.numBt4);
+            this.Controls.Add(this.numBt3);
+            this.Controls.Add(this.numBt2);
+            this.Controls.Add(this.numBt1);
+            this.Controls.Add(this.resultBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button numBt1;
+        private System.Windows.Forms.Button numBt2;
+        private System.Windows.Forms.Button numBt3;
+        private System.Windows.Forms.Button numBt4;
+        private System.Windows.Forms.Button numBt5;
+        private System.Windows.Forms.Button numBt6;
+        private System.Windows.Forms.Button numBt7;
+        private System.Windows.Forms.Button numBt8;
+        private System.Windows.Forms.Button numBt9;
+        private System.Windows.Forms.Button numBt0;
+        private System.Windows.Forms.Button plusBt;
+    }
+}
+
---
+++ calculator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public int res = 0;
+        public bool isNewNum = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numBt1_Click(object sender, EventArgs e)
+        {
+            setNum("1");
+        }
+
+        public void setNum(string num)
+        {
+            if (isNewNum)
+            {
+                resultBox.Text = num;
+                isNewNum = false;
+            }
+            else if (resultBox.Text == "0")
+            {
+                resultBox.Text = num;
+            }
+            else
+            {
+                resultBox.Text += num;
+            }
+        }
+
+        private void numBt2_Click(object sender, EventArgs e)
+        {
+            setNum("2");
+        }
+
+        private void plusBt_Click(object sender, EventArgs e)
+        {
+            int nowNum = int.Parse(resultBox.Text);
+            res += nowNum;
+            resultBox.Text = res.ToString();
+            isNewNum = true;
+        }
+    }
+}
